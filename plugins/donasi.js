958ff7524ae27b02dd57f5800289603c9d416268
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-07-15 20:39:48 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,16 +1,8 @@
 const qrku = "https://i.ibb.co/MnbqZ0N/qr-Aguz-Familia-07-06-22-1654614928.png"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
-╭─「 Donasi • Dana 」
-│ • Telkomsel [081320170984]
-│ • Dana  [081320170984]
-╰────
-╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6281320170984
-│ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
-│ _24jam tanpa kendala_
-╰────
+╭─「 𝕭𝖔𝖙 • Andii ༢࿔ྀ」
+ Yang Saya Butuhkan Hati Kamu Ka , Bukan Pulsa 🤧
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['about']
